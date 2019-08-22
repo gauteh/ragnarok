81e21bae46df52bccbe99779a57d65a124351146
Gaute Hope <eg@gaute.vetsj.com>
2019-08-22 20:14:14 +0200

    separate threads + add all fields

--- src/main.rs
+++ src/main.rs
@@ -8,70 +8,10 @@ use actix_cors::Cors;
 extern crate log;
 use env_logger;
 
-use notmuch;
-use bytes::Bytes;
 extern crate futures;
-use futures::stream::iter_ok;
-use serde_derive::Serialize;
-use std::sync::Arc;
-use itertools::Itertools;
 
 mod test;
-
-#[derive(Serialize)]
-pub struct Thread {
-    id: String,
-    authors: Vec<String>,
-    subject: String,
-    date: i64,
-}
-
-pub struct Threads (notmuch::Threads<'static, 'static>);
-
-impl Threads {
-    pub fn new (q: String) -> Threads {
-        let db = Arc::new (notmuch::Database::open (
-                &String::from("/Users/gauteh/.mail"),
-                notmuch::DatabaseMode::ReadOnly).unwrap());
-
-        debug! ("threads query: {}..", q);
-        let query = Arc::new (notmuch::Query::create (db.clone (), &q).unwrap ());
-
-        let threads =
-            <notmuch::Query<'static> as notmuch::QueryExt>
-                ::search_threads (query.clone()).unwrap ();
-
-        Threads (threads)
-    }
-}
-
-impl Iterator for Threads {
-    type Item = Thread;
-
-    fn next (&mut self) -> Option<Thread> {
-        match self.0.next() {
-            Some (t) => Some (Thread {
-                id: t.id (),
-                authors: t.authors(),
-                subject: t.subject(),
-                date: t.oldest_date() }),
-            _ => None
-        }
-    }
-}
-
-fn mthreads(req: HttpRequest) -> HttpResponse {
-    let query: String = req.match_info().get ("query").unwrap_or ("*").parse ().unwrap ();
-    HttpResponse::Ok ()
-        .content_type ("application/x-ndjson")
-        .streaming (
-            iter_ok::<_, ()> (
-                Threads::new (query)
-                .map (|th| Bytes::from(serde_json::ser::to_string (&th).unwrap()))
-                .intersperse (Bytes::from("\n"))
-            )
-        )
-}
+mod threads;
 
 fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "hypocloid=debug,actix_web=info");
@@ -90,8 +30,8 @@ fn main() -> std::io::Result<()> {
                     .max_age(3600),
             )
             .wrap (middleware::Logger::default())
-            .route("/threads", web::get().to_async (mthreads))
-            .route("/threads/{query}", web::get().to_async (mthreads))
+            .route("/threads", web::get().to_async (threads::threads))
+            .route("/threads/{query}", web::get().to_async (threads::threads))
     })
     .bind("127.0.0.1:8088")?
     .run()
