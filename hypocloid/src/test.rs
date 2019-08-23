89ea6266457f9dbf96c078641e919aab7ddbbaa0
Gaute Hope <eg@gaute.vetsj.com>
2019-08-23 11:28:33 +0200

    sw = 2

--- hypocloid/src/test.rs
+++ hypocloid/src/test.rs
@@ -1,61 +1,61 @@
 #[cfg(test)]
 
 mod test_notmuch {
-    fn init () {
-        std::env::set_var("RUST_LOG", "hypocloid=debug,actix_web=info");
-        let _ = env_logger::builder().is_test (true).try_init ();
-    }
+  fn init () {
+    std::env::set_var("RUST_LOG", "hypocloid=debug,actix_web=info");
+    let _ = env_logger::builder().is_test (true).try_init ();
+  }
 
-    #[test]
-    fn test_notmuch () {
-        init ();
+  #[test]
+  fn test_notmuch () {
+    init ();
 
-        let mail_path = "/Users/gauteh/.mail";
-        let db = notmuch::Database::open (&mail_path, notmuch::DatabaseMode::ReadOnly).unwrap();
+    let mail_path = "/Users/gauteh/.mail";
+    let db = notmuch::Database::open (&mail_path, notmuch::DatabaseMode::ReadOnly).unwrap();
 
-        let query = db.create_query ("*").unwrap();
-        let threads = query.search_threads().unwrap();
+    let query = db.create_query ("*").unwrap();
+    let threads = query.search_threads().unwrap();
 
-        let mut i = 0;
-        debug! ("counting threads..");
-        for _t in threads {
-            i += 1;
+    let mut i = 0;
+    debug! ("counting threads..");
+    for _t in threads {
+      i += 1;
 
-            if i % 500 == 0 {
-                debug! ("entries: {}", i);
-            }
-
-            if i > 20000 {
-                break;
-            }
-        }
-
+      if i % 500 == 0 {
         debug! ("entries: {}", i);
+      }
+
+      if i > 20000 {
+        break;
+      }
     }
 
-    #[test]
-    fn test_heap_iterator () {
-        init ();
+    debug! ("entries: {}", i);
+  }
 
-        use super::super::*;
-        use threads::*;
+  #[test]
+  fn test_heap_iterator () {
+    init ();
 
-        let threads = Threads::new (String::from ("*"));
+    use super::super::*;
+    use threads::*;
 
-        let mut i = 0;
-        debug! ("counting threads..");
-        for _t in threads {
-            i += 1;
+    let threads = Threads::new (String::from ("*"));
 
-            if i % 500 == 0 {
-                debug! ("entries: {}", i);
-            }
+    let mut i = 0;
+    debug! ("counting threads..");
+    for _t in threads {
+      i += 1;
 
-            if i > 20000 {
-                break;
-            }
-        }
-
+      if i % 500 == 0 {
         debug! ("entries: {}", i);
+      }
+
+      if i > 20000 {
+        break;
+      }
     }
+
+    debug! ("entries: {}", i);
+  }
 }
