ffaa573d688ceef21ed134a86fa0ad00c19c4ed5
Gaute Hope <eg@gaute.vetsj.com>
2019-09-08 20:00:02 +0200

    messages: handle error in notmuch-show

--- hypocloid/src/messages.rs
+++ hypocloid/src/messages.rs
@@ -1,7 +1,8 @@
 use super::*;
 use bytes::Bytes;
 
-pub fn messages(req: HttpRequest) -> HttpResponse
+pub fn messages(req: HttpRequest)
+  -> actix_web::Result<HttpResponse>
 {
   let route = "/messages";
   let mut query = &req.path()[route.len()..];
@@ -13,17 +14,23 @@ pub fn messages(req: HttpRequest) -> HttpResponse
 
   /* XXX: we presumably need to implement something custom here.
    * also, tokio_process has an async Command which would be good
-   * too. */
+   * too.
+   *
+   * XXX: sanitize html here, using e.g. ammonia?
+   *
+   * */
   let messages = Command::new ("notmuch")
     .arg ("show")
     .arg ("--format=json")
     .arg ("--exclude=false")
     .arg ("--include-html")
     .arg (query)
-    .output ().unwrap();
+    .output ()
+    .map_err (
+      |e| actix_web::error::ErrorInternalServerError (e))?;
 
-  HttpResponse::Ok ()
+  Ok(HttpResponse::Ok ()
     .content_type ("application/json")
-    .body (Bytes::from(messages.stdout))
+    .body (Bytes::from(messages.stdout)))
 }
 
