617dd054d9c326362df9ecc9eca5d5790a237c53
Gaute Hope <eg@gaute.vetsj.com>
2020-08-05 12:30:05 +0200

    try yew

--- astroid/src/lib.rs
+++ astroid/src/lib.rs
@@ -1,66 +1,67 @@
-// (Lines like the one below ignore selected Clippy rules
-//  - it's useful when you want to check your code with `cargo make verify`
-// but some rules are too "annoying" or are not applicable for your case.)
-#![allow(clippy::wildcard_imports)]
+#![recursion_limit="1024"]
+use wasm_bindgen::prelude::*;
+use yew::prelude::*;
 
-use seed::{prelude::*, *};
-
-// ------ ------
-//     Init
-// ------ ------
-
-// `init` describes what should happen when your app started.
-fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
-    Model::default()
+struct Model {
+    link: ComponentLink<Self>,
+    value: i64,
 }
 
-// ------ ------
-//     Model
-// ------ ------
-
-// `Model` describes our app state.
-type Model = i32;
-
-// ------ ------
-//    Update
-// ------ ------
-
-// (Remove the line below once any of your `Msg` variants doesn't implement `Copy`.)
-#[derive(Copy, Clone)]
-// `Msg` describes the different events you can modify state with.
 enum Msg {
-    Increment,
+    AddOne,
 }
 
-// `update` describes how to handle each `Msg`.
-fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
-    match msg {
-        Msg::Increment => *model += 1,
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Self {
+            link,
+            value: 0,
+        }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::AddOne => self.value += 1
+        }
+        true
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        // Should only return "true" if new properties are different to
+        // previously received properties.
+        // This component has no properties so we will always return "false".
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <div>
+                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
+                <p>{ self.value }</p>
+                {
+                    if self.value > 2 {
+                        html! {
+                            <table>
+                            { (0..10000).map(|_| html! {
+                                                           <tr>
+                                                               <td>{"A"}</td>
+                                                               <td>{"B"}</td>
+                                                               <td>{"V"}</td>
+                                                               </tr>
+                                                       }).collect::<Html>()
+                            }
+                            </table>
+                        }
+                    } else { html! {} }
+                }
+            </div>
+        }
     }
 }
 
-// ------ ------
-//     View
-// ------ ------
-
-// (Remove the line below once your `Model` become more complex.)
-#[allow(clippy::trivially_copy_pass_by_ref)]
-// `view` describes what to display.
-fn view(model: &Model) -> Node<Msg> {
-    div![
-        "This is a counter: ",
-        C!["counter"],
-        button![model, ev(Ev::Click, |_| Msg::Increment),],
-    ]
-}
-
-// ------ ------
-//     Start
-// ------ ------
-
-// (This function is invoked by `init` function in `index.html`.)
 #[wasm_bindgen(start)]
-pub fn start() {
-    // Mount the `app` to the element with the `id` "app".
-    App::start("app", init, update, view);
+pub fn run_app() {
+    App::<Model>::new().mount_to_body();
 }
