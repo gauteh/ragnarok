45d3fc838f333fe59240f4a50daa5ccfa857026b
Gaute Hope <eg@gaute.vetsj.com>
2021-10-11 07:56:27 +0200

    tags: simplify

--- hypocloid/src/tags.rs
+++ hypocloid/src/tags.rs
@@ -3,45 +3,19 @@ use std::convert::Infallible;
 use std::sync::Arc;
 use warp::{http::Response, reply::Reply, Filter};
 
-pub struct Tags;
-
-impl Tags {
-    pub fn new(db: String) -> Vec<String> {
-        let db = Arc::new(notmuch::Database::open(&db, notmuch::DatabaseMode::ReadOnly).unwrap());
-
-        debug!("getting all tags");
-
-        let tags: Vec<String> = db.all_tags().unwrap().collect();
-
-        tags
-    }
-}
-
 pub mod handlers {
     use super::*;
 
-    pub async fn query(state: Arc<HypoState>) -> Result<impl warp::Reply, Infallible> {
-        let nmdb = state
+    pub async fn all(state: Arc<HypoState>) -> Result<impl warp::Reply, Infallible> {
+        debug!("getting all tags");
+
+        let db = state
             .notmuch_config
             .get_from(Some("database"), "path")
             .unwrap();
 
-        let tags = Tags::new(String::from(nmdb));
-
-        let json = serde_json::to_string(&tags);
-
-        match json {
-            Ok(v) => Ok(Response::builder()
-                .header("Content-Type", "application/json")
-                .body(v)
-                .into_response()),
-
-            Err(_) => Ok(warp::http::StatusCode::BAD_REQUEST.into_response()),
-        }
-    }
-
-    pub async fn all(state: Arc<HypoState>) -> Result<impl warp::Reply, Infallible> {
-        query(state).await
+        let db = Arc::new(notmuch::Database::open(&db, notmuch::DatabaseMode::ReadOnly).unwrap());
+        Ok(warp::reply::json(&db.all_tags().unwrap().collect::<Vec<String>>()))
     }
 }
 
