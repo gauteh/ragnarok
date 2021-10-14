d695840d8dfe25de1134db7c0541eaa3e28dd38d
Gaute Hope <eg@gaute.vetsj.com>
2021-10-14 08:30:07 +0200

    tests: make fast

--- hypocloid/src/threads.rs
+++ hypocloid/src/threads.rs
@@ -143,11 +143,7 @@ mod test_notmuch {
         for _t in threads {
             i += 1;
 
-            if i % 500 == 0 {
-                debug!("entries: {}", i);
-            }
-
-            if i > 20000 {
+            if i > 20 {
                 break;
             }
         }
@@ -172,11 +168,7 @@ mod test_notmuch {
         for _t in threads {
             i += 1;
 
-            if i % 500 == 0 {
-                debug!("entries: {}", i);
-            }
-
-            if i > 20000 {
+            if i > 20 {
                 break;
             }
         }
