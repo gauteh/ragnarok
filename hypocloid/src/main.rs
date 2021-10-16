3dd984eaf8dda85370a4629034f7912181ef4b41
Yo'av Moshe <bjesus@gmail.com>
2021-10-16 16:28:36 +0200

    add a --allow-cors flag to hypocloid that allows `http://localhost:3000`
    this flag makes loading the interface in the browser easier.
    
    it still needs some work, mainly around:
    - refactor to make the code less repetitive
    - allow setting a different host than the hard-coded one

--- hypocloid/src/main.rs
+++ hypocloid/src/main.rs
@@ -5,8 +5,9 @@
 extern crate log;
 
 use env_logger::Env;
+use std::env;
 use std::sync::Arc;
-use warp::Filter;
+use warp::{http::Method, Filter};
 
 /* internal modules */
 mod messages;
@@ -29,13 +30,30 @@ async fn main() -> anyhow::Result<()> {
     let messages = messages::filters::messages(state.clone());
     let tags = tags::filters::all(state.clone());
 
-    let api = messages
-        .or(threads)
-        .or(tags)
-        .with(warp::log("hypocloid::api"));
+    let args: Vec<String> = env::args().collect();
+    let cors = warp::cors()
+        .allow_origin("http://localhost:3000")
+        .allow_methods(&[Method::GET, Method::POST, Method::DELETE]);
+    let mut is_cors = false;
 
-    info!("Listening on 127.0.0.1:8088");
-    warp::serve(api).run(([127, 0, 0, 1], 8088)).await;
+    if args.iter().any(|i| i == "--allow-cors") {
+        is_cors = true;
+    }
 
+    if is_cors {
+        let api = messages
+            .or(threads)
+            .or(tags)
+            .with(cors)
+            .with(warp::log("hypocloid::api"));
+        info!("Listening on 127.0.0.1:8088, CORS enabled");
+        warp::serve(api).run(([127, 0, 0, 1], 8088)).await;
+    } else {
+        let api = messages
+            .or(threads)
+            .or(tags)
+            .with(warp::log("hypocloid::api"));
+        warp::serve(api).run(([127, 0, 0, 1], 8088)).await;
+    }
     Ok(())
 }
